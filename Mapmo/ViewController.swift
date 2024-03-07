7c1400da25526f569a9761204913008685e3353e
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-03-07 19:37:42 +0900

    Initial Commit

---
+++ Mapmo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mapmo
+//
+//  Created by 조유진 on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
